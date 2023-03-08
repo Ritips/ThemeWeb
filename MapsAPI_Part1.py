7924df66dab589e34bd783f90946355cc85e448c
StormVortex <undem1390@gmail.com>
2023-03-08 21:27:56 +0300

    part4

--- MapsAPI_Part1.py
+++ MapsAPI_Part1.py
@@ -17,6 +17,7 @@ class ShowMap(QWidget):
 
         self.pixmap = QPixmap()
         self.default_toponym_point = self.toponym_point = self.toponym = self.spn = self.default_spn = None
+        self.par_l = 'map'
         self.get_name = ''
         self.scale_size = 0.05
         self.scale_ll = 0.05
@@ -35,6 +36,23 @@ class ShowMap(QWidget):
         self.button_scale_ll.setGeometry(650, 170, 150, 50)
         self.button_scale_ll.clicked.connect(self.set_scale_ll)
         self.button_scale_ll.setFocusPolicy(Qt.NoFocus)
+
+        self.button_spn = QPushButton("Set Scheme", self)
+        self.button_spn.setGeometry(650, 230, 150, 50)
+        self.button_spn.clicked.connect(self.set_par_l)
+        self.button_spn.setFocusPolicy(Qt.NoFocus)
+
+    def set_par_l(self):
+        sat_options = ['sat', 'sat,skl', 'sat,trf,skl', 'sat,trf']
+        map_options = ['map', 'map,skl', 'map,skl,trf', 'map,trf']
+        options = map_options + sat_options
+        valid = QInputDialog.getItem(self, 'Scheme', 'Sat Scheme', options)
+        if valid[1]:
+            self.par_l = valid[0]
+        else:
+            self.par_l = 'map'
+        if self.get_name:
+            self.get_image()
 
     def set_scale(self):
         valid = QInputDialog.getText(self, 'Scale', 'SetScale')
@@ -114,14 +132,15 @@ class ShowMap(QWidget):
         self.get_spn()
         self.get_image()
 
-    def get_image(self, toponym_point=None, par_l='map'):
-        toponym_point = self.toponym_point if not toponym_point else toponym_point
+    def get_image(self):
+        toponym_point = self.toponym_point
+        par_l = self.par_l
         delta = self.spn
         map_params = {
             "ll": f"{toponym_point[0]},{toponym_point[1]}",
             "spn": f"{delta[0]},{delta[1]}",
             "l": par_l,
-            "pt": f"{toponym_point[0]},{toponym_point[1]},round"
+            "pt": f"{self.default_toponym_point[0]},{self.default_toponym_point[1]},round"
         }
         map_api_server = "http://static-maps.yandex.ru/1.x/"
         response = requests.get(map_api_server, params=map_params)
@@ -148,6 +167,7 @@ class ShowMap(QWidget):
 
     def get_coordinates(self, toponym):
         self.toponym_point = tuple(map(float, toponym["Point"]["pos"].split()))
+        self.default_toponym_point = self.toponym_point
 
     def get_spn(self):
         envelope = self.toponym["boundedBy"]["Envelope"]
