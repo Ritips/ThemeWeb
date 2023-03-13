b4f3a24b5279e6f0b4ab97d38f2fccb6511ccf8d
StormVortex <undem1390@gmail.com>
2023-03-13 21:52:45 +0300

    TryOnSomeSimpleThingsOfHTML2

---
+++ ColonisationPlanet2.py
@@ -0,0 +1,43 @@
+from flask import url_for, Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/index/<title>/<prof>')
+def first_sample(title, prof):
+    param = dict()
+    param["title"] = title
+    param["prof"] = prof
+    if prof == "physician":
+        param["photo"] = url_for('static', filename=f'img/Mars.jpg')
+    elif prof == "engineer":
+        param["photo"] = url_for('static', filename=f'img/saved_image.jpg')
+    return render_template('base.html', **param)
+
+
+@app.route('/list_prof/<list_param>')
+def list_prof(list_param):
+    param = dict()
+    param["profs"] = ["engineer", "physician"]
+    param["title"] = ["Professions"]
+    param["par"] = list_param
+    return render_template("task3.html", **param)
+
+
+@app.route('/answer')
+@app.route('/auto_answer')
+def auto_answer():
+    param = dict()
+    param["title"] = "Questionnaire"
+    param["surname"] = 'unknown'
+    param["name"] = 'unknown'
+    param["education"] = 'unknown'
+    param["profession"] = 'unknown'
+    param["sex"] = 'unknown'
+    param["motivation"] = 'unknown'
+    param["ready"] = 'unknown'
+    return render_template("auto_answer.html", **param)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
