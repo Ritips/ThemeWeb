09302bedbb5c5dfee6afa0f89e7a40c54fc676f6
StormVortex <undem1390@gmail.com>
2023-04-09 20:16:05 +0300

    sqlalchemy learning 4.1.1

--- forms/mars_job.py
+++ forms/mars_job.py
@@ -1,13 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, BooleanField, IntegerField, StringField, DateTimeField
 from wtforms.validators import DataRequired
+from datetime import datetime as dt
 
 
 class JobForm(FlaskForm):
     job = StringField("Job Title", validators=[DataRequired()])
     team_leader = IntegerField("Team leader's id", validators=[DataRequired()])
     collaborators = StringField("Collaborators", validators=[DataRequired()])
-    start_date = DateTimeField("Start date(format: %Y-%m-%d %H:%M:%S)", validators=[DataRequired()])
+    start_date = DateTimeField("Start date(format: %Y-%m-%d %H:%M:%S)", default=dt.now())
     work_size = IntegerField("Work size", validators=[DataRequired()])
     is_finished = BooleanField("Is finished")
     submit = SubmitField("Confirm")
