d648e1b516a9abec419af638c629f66e0442619a
StormVortex <undem1390@gmail.com>
2023-04-09 20:17:02 +0300

    sqlalchemy learning 4.1.1

--- forms/mars_job.py
+++ forms/mars_job.py
@@ -8,7 +8,7 @@ class JobForm(FlaskForm):
     job = StringField("Job Title", validators=[DataRequired()])
     team_leader = IntegerField("Team leader's id", validators=[DataRequired()])
     collaborators = StringField("Collaborators", validators=[DataRequired()])
-    start_date = DateTimeField("Start date(format: %Y-%m-%d %H:%M:%S)", default=dt.now())
+    start_date = DateTimeField("Start date(format: %Y-%m-%d %H:%M:%S)", default=dt.now(), validators=[DataRequired()])
     work_size = IntegerField("Work size", validators=[DataRequired()])
     is_finished = BooleanField("Is finished")
     submit = SubmitField("Confirm")
